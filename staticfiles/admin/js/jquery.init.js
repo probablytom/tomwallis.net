7cd923bfc5c4cd5f4e532882ecc33235e9e5140f
Tom Wallis <tom@tom-wallis.com>
2015-02-17 22:51:58 +0000

    Refactoring again

---
+++ staticfiles/admin/js/jquery.init.js
@@ -0,0 +1,1 @@
+/Users/tom/Documents/venvs/tomwallis.net/lib/python2.7/site-packages/django/contrib/admin/static/admin/js/jquery.init.js
--- tw_net/settings.py
+++ tw_net/settings.py
@@ -81,8 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
 # Template setup
 TEMPLATE_DIRS = (
-	'templates/',		
+	'templates/',
 )
