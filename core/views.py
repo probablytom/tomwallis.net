d53cc56469ffc4314804fecb8b9e88b71fd6e256
Tom Wallis <tom@tom-wallis.com>
2015-03-16 13:56:04 +0000

    Working projects page\!

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from core.models import Post
+from core.models import Post, Project
 
 def home(request):
     return render(request, 'home.html', {'posts': Post.objects.all()})
 
-
+def projects(request):
+    return render(request, 'projects.html', {'projects': Project.objects.all()})
